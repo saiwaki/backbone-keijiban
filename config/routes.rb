0f5ac204aa4ca479ac9feceb4d8a232d8cd71ff7
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-05-11 18:13:50 +0900

    Boardのルーティングを定義する
    boardのルーティングを定義します。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :boards, format: :json
+
   root to: 'main#index'
 
   # The priority is based upon order of creation: first created -> highest priority.

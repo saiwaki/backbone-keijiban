1bce0f1ff17f2358951db658f51721ee7c754f4c
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-05-11 21:33:21 +0900

    comments#create を実装する

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,23 @@ class CommentsController < ApplicationController
     @board = Board.find(params[:board_id])
     @comments = @board.comments
   end
+
+  def create
+    @board = Board.find(params[:board_id])
+    @comment = @board.comments.new(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.json {render json: @comment}
+      else
+        format.json {render json: @comment.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :content)
+  end
+
 end

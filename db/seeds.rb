30d984a67c3a41e30c4757e415e112de2573162c
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-05-11 17:11:19 +0900

    Fabricator, Fakerを使ってseedsファイルを記述し、rake db:seedする
    Fabricator, Fakerという便利gemを使って、テストデータを挿入します。記述したら、$ rake db:seedします。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Fabricator(:board) do
+  name {Faker::Name.title}
+end
+
+10.times {Fabricate(:board)}

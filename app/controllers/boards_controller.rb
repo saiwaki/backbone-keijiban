1f3f9a08dcdc89034a18b79a2a35d4f785b76cc4
Hitoshi Saiwaki <hitoshi.ee@gmail.com>
2015-05-11 19:41:11 +0900

    boards#create を実装する
    まず、strong parameters を実装する。それを使って、createメソッドを実装、if文でcreate処理を分岐する。

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,21 @@ class BoardsController < ApplicationController
   def show
     @board = Board.find(params[:id])
   end
+
+  def create
+    @board = Board.new(board_params)
+    respond_to do |format|
+      if @board.save
+        format.json {render json: @board}
+      else
+        format.json {render json: @board.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name)
+  end
 end
